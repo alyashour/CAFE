16606a109344e64300fb55e64a976a49f47e938c
Aly <alyashour1@gmail.com>
2024-03-07 20:45:53 -0500

    Initial Commit

---
+++ CAFE/CAFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAFEApp.swift
+//  CAFE
+//
+//  Created by Aly Ashour on 2024-03-07.
+//
+
+import SwiftUI
+
+@main
+struct CAFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CAFE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CAFE
+//
+//  Created by Aly Ashour on 2024-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
